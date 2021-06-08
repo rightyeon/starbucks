e6dbc67d6b74da5d9e0f049ec114299bb644822e
rightyeon <bacchus.xd@gmail.com>
2021-06-08 17:37:49 +0900

    throttle lodash cdn, js animation library-gsap

--- js/main.js
+++ js/main.js
@@ -16,3 +16,28 @@ searchInput.addEventListener('blur',function(){
     searchInput.setAttribute('placeholder','');
     searchInput.value = "";
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        // hide badge
+        gsap.to(badgeEl, .6, {
+            opacity:0,
+            display:'none'
+            // 투명도만 0으로 낮추면 사라진 것이 아니라 영역이 잡힌다.
+        });
+    }
+    else {
+        // show badge
+        gsap.to(badgeEl, .6, {
+            opacity:1,
+            display:'block'
+        })
+    }
+}),300);
+
+// gsap.to(요소, 지속시간, 옵션);
+// _.throttle(함수, 시간);

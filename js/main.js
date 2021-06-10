96eda51a58b95c9d5c6555851e25901f2b922c8f
rightyeon <bacchus.xd@gmail.com>
2021-06-10 15:45:30 +0900

    starbucks promotion slide contents

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const searchEl = document.querySelector('.search');
-const searchInput = searchEl.querySelector('input');
+const searchEl      = document.querySelector('.search');
+const searchInput   = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', function(){
     searchInput.focus();
@@ -50,4 +50,47 @@ fadeEls.forEach(function(el, idx){
         delay: (idx + 1) * .7, //0.7, 1.4, 2.1, 2.7...
         opacity: 1,
     })
+});
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay:true,
+    loop:true
+});
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView : 3,      // 한번에 보여줄 갯수
+    spaceBetween : 10,      // 슬라이드 사이 여백
+    centeredSlides: true,   // 첫번째 슬라이드가 가운데로 
+    loop:true,
+    autoplay: {
+        delay: 3000
+    },
+    pagination : {
+        el:'.promotion .swiper-pagination',
+        clickable:true  // 사용자 페이지 번호 요소 제어 기능 추가
+    },
+    navigation : {
+        prevEl : '.promotion .swiper-prev',
+        nextEl : '.promotion .swiper-next'
+    }
+});
+
+// PROMOTION AREA
+const promotionEl           = document.querySelector('.promotion');
+const promotionToggleBtn    = document.querySelector('.toggle-promotion');
+let isHidePromotion         = false;
+
+promotionToggleBtn.addEventListener('click', function(){
+    isHidePromotion = !isHidePromotion;
+    if (isHidePromotion) {
+        // hide
+        promotionEl.classList.add('hide');
+    } else {
+        // show
+        promotionEl.classList.remove('hide');
+    }
+
 });
